238497dfc32f778d64d36aa02f117a63ae18ae5f
Abdul-Waahab <abdulwahab90bscs18@gmail.com>
2022-08-31 19:23:24 +0500

    permit implemetation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  # member_signed_in?
-  # current_member
-  # member_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+	protect_from_forgery with: :exception
+
+	protected
+	
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+	end
+
 end
